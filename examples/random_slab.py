222cd2063dbe8a78087f4a4f1a0b51a605b6552b
KingSpawn15 <36850409+KingSpawn15@users.noreply.github.com>
2025-06-25 18:15:51 +0000

    run now

--- examples/random_slab.py
+++ examples/random_slab.py
@@ -235,8 +235,9 @@ def run_sim(rot_angle=0):
         cell_size=cell_size,
         resolution=resolution,
         boundary_layers=pml_layers,
-        # epsilon_input_file='exported_epsilon_random_90.h5',
-        geometry = choi_2011_geometry_slab(width_k0 = 50, sizez_k0 = 100, seed  = 42),
+        epsilon_input_file='exported_epsilon_random_90.h5',
+        eps_averaging = True,
+        # geometry = choi_2011_geometry_slab(width_k0 = 50, sizez_k0 = 100, seed  = 42),
         force_complex_fields=True,
         sources=sources,
         k_point=k_point,
@@ -282,9 +283,9 @@ def run_sim(rot_angle=0):
 if __name__ == "__main__":
 
 
-    export_geometry(0)  # Export the geometry to a file
+    # export_geometry(0)  # Export the geometry to a file
 
-    # results = run_sim(0)  # Example rotation angle of 45 degrees
+    results = run_sim(0)  # Example rotation angle of 45 degrees
     # # plot_sim_results(results)    
 
     # if rank == 0:
