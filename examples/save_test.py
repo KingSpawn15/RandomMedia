94a0c4deff00a3e787869f30442c2737e35e0473
KingSpawn15 <36850409+KingSpawn15@users.noreply.github.com>
2025-06-26 20:12:08 +0000

    added run

--- examples/save_test.py
+++ examples/save_test.py
@@ -55,5 +55,63 @@ def save_file():
     simx.init_sim()
     simx.dump("examples/test/",single_parallel_file=True)
 
+def run_sim(rot_angle):
+
+    n = 1
+    wavelength = 0.6
+    fsrc = 1/wavelength
+    rot_angle = np.deg2rad(rot_angle)  # Convert angle to radians
+    k0 = 2 * np.pi / wavelength  # Wave number
+    kp = mp.Vector3(fsrc * n).rotate(mp.Vector3(z=1), rot_angle)
+
+    sources_c = [
+        mp.EigenModeSource(
+            src=mp.GaussianSource(fsrc, fwidth=fsrc/7, is_integrated=True),
+            # src=mp.ContinuousSource(fsrc),
+            amplitude=1.0,
+            center=mp.Vector3(-2, 0, 0),
+            size=mp.Vector3(y=cell_y),
+            direction=mp.AUTOMATIC if rot_angle == 0 else mp.NO_DIRECTION,
+            eig_kpoint=kp,
+            eig_band=1,
+            eig_parity=mp.EVEN_Y + mp.ODD_Z if rot_angle == 0 else mp.ODD_Z,
+            eig_match_freq=True,
+        )
+    ]
+
+    sim = mp.Simulation(
+        cell_size=cell_size,
+        resolution=resolution,
+        dimensions = 2,
+        boundary_layers=pml_layers,
+        geometry=[
+            mp.Block(
+                center=mp.Vector3(0, 0, 0),
+                size=mp.Vector3(2, 0.5, mp.inf),
+                material=mp.Medium(epsilon=4))],
+        # epsilon_input_file="exported_epsilon_test.h5",
+        sources=sources_c,
+        k_point=kp,
+        default_material=default_material
+    )
+
+    sim.init_sim()
+
+    # sim.load_structure("examples/sd.h5", single_parallel_file=False)
+    sim.load("examples/test/", single_parallel_file = False)
+    sim.change_sources(sources_c)
+    sim.change_k_point(kp)
+
+    # # Add DFT monitor for field extraction
+    # sim.init_sim()
+    ez_freq = sim.add_dft_fields([mp.Ez], fsrc, 0, 1, where=mp.Volume(center=mp.Vector3(0,0,0), size=cell_size))
+    # Run simulation
+
+    sim.run(until=int(1000/fsrc))
+
+    # Extract Ez field data
+    ez_val = sim.get_dft_array(ez_freq, mp.Ez, 0)
+
 if __name__ == "__main__":
-    save_file()
+    # save_file()
+    run_sim(0)
