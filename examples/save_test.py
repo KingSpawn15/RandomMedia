32ecfac67f84a2ee9cbc543ddcf5d3cf33e51763
KingSpawn15 <36850409+KingSpawn15@users.noreply.github.com>
2025-06-26 18:28:11 +0000

    Update file dump path and change single_parallel_file option in save_test.py

--- examples/save_test.py
+++ examples/save_test.py
@@ -53,7 +53,7 @@ def save_file():
 
     # simx.run(until=0)
     simx.init_sim()
-    simx.dump("examples/",single_parallel_file=True)
+    simx.dump("examples/test/",single_parallel_file=False)
 
 if __name__ == "__main__":
     save_file()
