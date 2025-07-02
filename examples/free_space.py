10d23826ed732243e4fe2a3f02d2016ba1a970dc
KingSpawn15 <36850409+KingSpawn15@users.noreply.github.com>
2025-07-02 17:57:50 +0000

    no k

--- examples/free_space.py
+++ examples/free_space.py
@@ -59,7 +59,7 @@ def run_sim(wavelength = 0.6, mesh_resolution = 40, source_amplitude = [1.0]):
         boundary_layers=pml_layers,
         # force_complex_fields=True,
         sources=sources,
-        k_point=mp.Vector3(fsrc * n).rotate(mp.Vector3(z=1), mode_to_angle(source_amplitude[1][1], wavelength = wavelength, L = cell_y)),
+        # k_point=mp.Vector3(fsrc * n).rotate(mp.Vector3(z=1), mode_to_angle(source_amplitude[1][1], wavelength = wavelength, L = cell_y)),
         default_material=default_material
     )
 
